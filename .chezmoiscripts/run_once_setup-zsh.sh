1e7cc321b7a8c97e6c2506381f0619fcc281f860
Misaka <chuxubank@qq.com>
2025-02-24 16:18:04 +0800

    fix(omz): check if ohmyzsh installed

--- .chezmoiscripts/run_once_setup-zsh.sh
+++ .chezmoiscripts/run_once_setup-zsh.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+[[ ! -d "$HOME/.oh-my-zsh" ]] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || true
