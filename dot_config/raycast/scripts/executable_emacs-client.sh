5d56db8f5e8a690bb96d82e792b04611f100249b
Misaka <chuxubank@qq.com>
2025-02-27 13:26:35 +0800

    chore(emacs): only create new frame when there's no frame

--- dot_config/raycast/scripts/executable_emacs-client.sh
+++ dot_config/raycast/scripts/executable_emacs-client.sh
@@ -14,4 +14,10 @@
 # @raycast.author Misaka
 # @raycast.authorURL https://github.com/chuxubank
 
-emacsclient -cn -a=""
+if emacsclient --eval '(> (length (frame-list)) 1)' 2>/dev/null | grep -q t; then
+    osascript -e 'tell application "Emacs" to activate'
+    echo "Bringing Emacs to the front..."
+else
+    emacsclient -cn -a=""
+    echo "Starting Emacs..."
+fi
