a5dfd1e97198599ae54262f5c91bf7915536ea53
Misaka <chuxubank@qq.com>
2025-03-01 12:24:53 +0800

    chore(init): fix pass prepare

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 if [ -d "$HOME/.oh-my-zsh" ]; then
-    echo "OMZ installed"
+    echo "✅ OMZ installed."
 else
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+# Check pass
 if type pass >/dev/null 2>&1; then
-    echo "Pass installed"
+    echo "✅ Pass installed."
 else
     case "$(uname -s)" in
     Darwin)
@@ -17,16 +18,48 @@ else
         # Add installation commands for Linux here
         ;;
     *)
-        echo "Unsupported OS"
+        echo "❌ Unsupported OS."
         exit 1
         ;;
     esac
 fi
 
-# Check if the pass repository is already cloned
 export PASSWORD_STORE_DIR="$HOME/Developer/Personal/pass"
 if [ -d "$PASSWORD_STORE_DIR" ]; then
-    echo "Pass dir exist"
+    echo "✅ Pass dir exist."
 else
     gh repo clone pass "$PASSWORD_STORE_DIR"
+fi
+
+# Check gpg
+if type gpg >/dev/null 2>&1; then
+    echo "✅ GPG installed."
+else
+    case "$(uname -s)" in
+    Darwin)
+        brew install gpg
+        ;;
+    Linux)
+        # Add installation commands for Linux here
+        ;;
+    *)
+        echo "❌ Unsupported OS."
+        exit 1
+        ;;
+    esac
+fi
+
+if gpg --list-secret-keys | grep -q "^sec"; then
+    echo "✅ GPG secret key found."
+else
+    echo "❌ No GPG secret key found."
+    while true; do
+        read -rp "🔑 Please enter the path to your GPG private key file: " key_file
+        if [ -f "$key_file" ]; then
+            echo "📥 Importing GPG key from $key_file..."
+            gpg --import --pinentry-mode=loopback "$key_file" && echo "✅ Key imported successfully!" && break
+        else
+            echo "⚠️ File not found. Please enter a valid path."
+        fi
+    done
 fi
