709a041318663aab85ec292f864192d4a025c863
Misaka <chuxubank@qq.com>
2025-03-03 21:25:00 +0800

    fix(init): use zsh prompt with complete

--- .scripts/setup-gpg.sh
+++ .scripts/setup-gpg.sh
@@ -6,9 +6,13 @@ if gpg --list-secret-keys | grep -q "^sec"; then
     echo "✅ gpg secret key found."
 else
     echo "❌ no gpg secret key found."
+    autoload -Uz compinit
+    compinit
     while true; do
+        key_file=""
         echo "🔑 Please enter the path to your GPG private key file: "
-        read -e key_file
+        vared -c -p "> " key_file
+        key_file=${key_file/#\~/$HOME}
         if [ -f "$key_file" ]; then
             echo "📥 Importing GPG key from $key_file..."
             gpg --import --pinentry-mode=loopback "$key_file" && echo "✅ Key imported successfully!" && break
