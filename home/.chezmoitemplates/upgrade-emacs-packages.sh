e896100d8261efaf03e4ddeef69241d5cef96845
Misaka <chuxubank@qq.com>
2025-08-08 08:58:03 +0800

    fix: simplify emacs package upgrade script
    The script should not attempt to kill the emacsclient, as it is not
    always running.

--- home/.chezmoitemplates/upgrade-emacs-packages.sh
+++ home/.chezmoitemplates/upgrade-emacs-packages.sh
@@ -1,5 +1,9 @@
+\emacs --fg-daemon --eval '(package-upgrade-all)' --eval '(kill-emacs)'
+
+\emacsclient --eval '(kill-emacs)' || true
+
 if \emacs --version 2>&1 | \grep -q 'Emacs Mac Port'; then
-    \emacs --fg-daemon --eval '(package-upgrade-all)' --eval '(kill-emacs)' && \emacsclient --eval '(kill-emacs)' && \emacs &
+    \emacs &
 else
-    \emacs --fg-daemon --eval '(package-upgrade-all)' --eval '(kill-emacs)' && \emacsclient -a '' --eval '(kill-emacs)' && \emacs --daemon
+    \emacsclient -cn -a=""
 fi
