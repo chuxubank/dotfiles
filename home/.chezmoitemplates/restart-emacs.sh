4a5d316be99cf628baf999f6ecaafb2b3f577ecc
Misaka <chuxubank@qq.com>
2025-10-10 23:11:06 +0800

    refactor(emacs): extract restart logic into template and simplify scripts
    Extract Emacs restart functionality into dedicated template file,
    simplifying main onchange script Update upgrade function to include
    restart step Standardize macOS detection in restart logic

---
+++ home/.chezmoitemplates/restart-emacs.sh
@@ -0,0 +1,14 @@
+# Kill existing Emacs session if running
+\emacsclient --eval '(kill-emacs)' >/dev/null 2>&1 || true
+
+# Start Emacs depending on OS
+{{ if eq .chezmoi.os "android" -}}
+termux-x11 :1 -xstartup "\emacs" &
+{{ else -}}
+env TERM_PROGRAM=
+if \emacs --version 2>&1 | \grep -q 'Emacs Mac Port'; then
+    \emacs &
+else
+    \emacsclient -cn -a=""
+fi
+{{ end -}}
--- home/.chezmoitemplates/upgrade-emacs-packages.sh
+++ home/.chezmoitemplates/upgrade-emacs-packages.sh
@@ -1,11 +1,3 @@
 CI=true
 
 \emacs --fg-daemon --eval '(package-upgrade-all)' --eval '(kill-emacs)'
-
-\emacsclient --eval '(kill-emacs)' || true
-
-if \emacs --version 2>&1 | \grep -q 'Emacs Mac Port'; then
-    \emacs &
-else
-    \emacsclient -cn -a=""
-fi
