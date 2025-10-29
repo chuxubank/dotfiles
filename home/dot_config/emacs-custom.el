4c33501da05a4892b1b7f0b18e6a64aaa24e8c72
Misaka <chuxubank@qq.com>
2025-10-29 23:02:27 +0800

    feat(emacs): disable mouse wheel progressive speed

--- home/dot_config/emacs-custom.el
+++ home/dot_config/emacs-custom.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mouse-wheel-progressive-speed nil)
  '(safe-local-variable-values
    '((org-highlight-latex-and-related) (org-blank-before-new-entry))))
 (custom-set-faces
