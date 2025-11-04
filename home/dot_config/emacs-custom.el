9fce2f9d0b6654a60bb9b8c879c906020bca0415
Misaka <chuxubank@qq.com>
2025-11-04 14:25:42 +0800

    chore(emacs): update package-selected-packages list
    Added multiple packages for Emacs functionality enhancements
    including: - Programming languages: Kotlin, Lua, Go, Rust, Swift,
    Protobuf - Editor enhancements: copilot, treesitter, sideline tools,
    codeium - Org-mode extensions: org-roam, org-anki, org-jira -
    Productivity tools: gptel, dashboards, ement, mu4e - Version
    control: magit-todos, code-review - Miscellaneous: docker, telega,
    beancount, diagrams

--- home/dot_config/emacs-custom.el
+++ home/dot_config/emacs-custom.el
@@ -5,6 +5,56 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(mouse-wheel-progressive-speed nil)
+ '(package-selected-packages
+   '(ace-pinyin activities age aider aidermacs anki-vocabulary apheleia
+                applescript-mode auctex avy-embark-collect bbdb beacon
+                beancount benchmark-init bing-dict breadcrumb buck
+                caddyfile-mode cape cdlatex chatgpt-shell
+                chezmoi-extensions citar-embark citar-org-roam
+                cmake-font-lock code-review codeium company-prescient
+                consult-gh-embark consult-gh-forge copilot
+                corfu-prescient dart-mode dashboard deft delight
+                devdocs diagram-preview diff-hl dired-rsync-transient
+                dirvish docker docker-compose-mode dockerfile-mode
+                doom-modeline doom-themes dumb-jump easy-hugo
+                eglot-java ejira ejira3 el-patch eldoc-box eldoc-toml
+                elfeed-org elfeed-protocol elisp-demos elogcat
+                emacs-everywhere ement emms eshell-vterm
+                exec-path-from-shell flycheck-gradle flycheck-kotlin
+                flycheck-plantuml flymake-ansible-lint flymake-bridge
+                flymake-gradle git-link git-timemachine glab gntp
+                go-mode go-translate goggles gogs gptel-magit
+                gptel-prompts gradle gradle-el groovy-mode gtea helm
+                helpful hugoista hydra-posframe igist
+                immersive-translate indent-bars jabber jira
+                kaolin-themes kdl-mode kotlin-mode kotlin-ts-mode kv
+                leetcode ligature logview lsp-proxy lua-mode
+                magit-todos major-mode-hydra makefile-executor
+                marginalia markdown-xwidget math-delimiters
+                meow-tree-sitter meow-vterm mermaid-mode minimap
+                mistty mu4e-column-faces mu4e-overview
+                nerd-icons-corfu nerd-icons-dired netease-cloud-music
+                no-littering nov ns-auto-titlebar ob-aider
+                ob-applescript ob-async ob-go ob-kotlin ob-powershell
+                ob-swiftui oj org-agenda-count org-anki org-appear
+                org-cliplink org-cv org-dial org-drill org-edna
+                org-imgtog org-jira org-link-beautify org-media-note
+                org-mode org-modern org-modern-indent org-pdftools
+                org-ref org-remoteimg org-roam-bibtex org-roam-ui
+                org-yt osm osx-dictionary osx-location osx-plist
+                ox-hugo ox-jira pangu-spacing pass password-store-menu
+                pet pinyin-isearch pinyin-search poly-ansible
+                powershell protobuf-mode pulsar quick-sdcv rainbow-csv
+                rainbow-mode rg rime rust-mode sideline-blame
+                sideline-eglot sideline-flycheck sideline-flymake
+                smartparens solaire-mode swift-ts-mode system-packages
+                telega templ-ts-mode templatel toc-mode
+                transpose-frame tree-sitter treemacs-magit
+                treemacs-nerd-icons treemacs-tab-bar treesit-auto
+                treesit-fold treesit-langs tributary ultra-scroll
+                undo-fu-session uv-mode valign vbscript-mode
+                vertico-prescient video-trimmer vterm-toggle vundo
+                winum yaml-pro))
  '(safe-local-variable-values
    '((org-highlight-latex-and-related) (org-blank-before-new-entry))))
 (custom-set-faces
