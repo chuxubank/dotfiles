b5dcdd20b86aa714df4cfc52961167a71904ac85
Misaka <chuxubank@qq.com>
2026-04-03 12:07:54 +0800

    chore(emacs): update package-selected-packages list
    Remove several packages (age, dashboard, eglot-java, el-patch,
    flymake-bridge, gntp, go-translate, gradle, hydra-posframe,
    immersive-translate, jabber, kv, org-jira, org-ref, ox-jira,
    quick-sdcv, sideline-blame, tree-sitter, oj) and add new ones
    (ejira3, jiralib3) while reformatting the list.

--- home/dot_config/emacs-custom.el
+++ home/dot_config/emacs-custom.el
@@ -6,52 +6,47 @@
  ;; If there is more than one, they won't work right.
  '(mouse-wheel-progressive-speed nil)
  '(package-selected-packages
-   '(ace-pinyin activities age agent-shell aider aidermacs
-                anki-vocabulary apheleia applescript-mode auctex
-                avy-embark-collect bbdb beacon beancount
-                benchmark-init bing-dict breadcrumb buck
-                caddyfile-mode cape cdlatex chatgpt-shell chezmoi
-                citar-embark citar-org-roam cmake-font-lock
-                code-review codeium consult-gh-embark consult-gh-forge
-                copilot corfu-prescient dart-mode dashboard deft
-                delight devdocs diagram-preview diff-hl
+   '(ace-pinyin activities agent-shell aider aidermacs anki-vocabulary
+                apheleia applescript-mode auctex avy-embark-collect
+                bbdb beacon beancount benchmark-init bing-dict
+                breadcrumb buck caddyfile-mode cape cdlatex
+                chatgpt-shell chezmoi citar-embark citar-org-roam
+                cmake-font-lock code-review codeium consult-gh-embark
+                consult-gh-forge copilot corfu-prescient dart-mode
+                deft delight devdocs diagram-preview diff-hl
                 dired-rsync-transient dirvish docker
                 docker-compose-mode dockerfile-mode doom-modeline
-                doom-themes dumb-jump easy-hugo edit-indirect
-                eglot-java el-patch eldoc-box eldoc-toml elfeed-org
-                elfeed-protocol elisp-demos elogcat emacs-everywhere
-                ement emms eshell-vterm exec-path-from-shell
-                flycheck-gradle flycheck-kotlin flycheck-plantuml
-                flymake-ansible-lint flymake-bridge flymake-gradle
-                git-link git-timemachine glab gntp go-mode
-                go-translate goggles gogs gptel-magit gptel-prompts
-                gradle gradle-el graphql-mode groovy-mode gt gtea
-                helpful hugoista hydra-posframe igist
-                immersive-translate indent-bars jabber kaolin-themes
-                kdl-mode kotlin-mode kotlin-ts-mode kv leetcode
-                ligature logview lsp-proxy lua-mode magit-todos
-                major-mode-hydra makefile-executor marginalia
-                markdown-xwidget math-delimiters mcp-server md-babel
-                meow-tree-sitter meow-vterm mermaid-mode minimap
-                mistty mu4e-column-faces mu4e-overview
-                nerd-icons-corfu nerd-icons-dired netease-cloud-music
-                no-littering nov ns-auto-titlebar ob-aider
-                ob-applescript ob-async ob-go ob-kotlin ob-powershell
-                ob-rust ob-swiftui ob-typescript oj org-agenda-count
-                org-anki org-appear org-cliplink org-cv org-dial
-                org-drill org-edna org-imgtog org-jira
-                org-link-beautify org-media-note org-mode org-modern
-                org-modern-indent org-pdftools org-ref org-remoteimg
-                org-roam-bibtex org-roam-ui org-yt osm osx-dictionary
-                osx-location osx-plist ox-hugo ox-jira ox-jira3
+                doom-themes dumb-jump easy-hugo edit-indirect ejira3
+                eldoc-box eldoc-toml elfeed-org elfeed-protocol
+                elisp-demos elogcat emacs-everywhere ement emms
+                eshell-vterm exec-path-from-shell flycheck-gradle
+                flycheck-kotlin flycheck-plantuml flymake-ansible-lint
+                flymake-gradle git-link git-timemachine glab go-mode
+                goggles gogs gptel-magit gptel-prompts gradle-el
+                graphql-mode groovy-mode gt gtea helpful hugoista
+                igist indent-bars jiralib3 kaolin-themes kdl-mode
+                kotlin-mode kotlin-ts-mode leetcode ligature logview
+                lsp-proxy lua-mode magit-todos major-mode-hydra
+                makefile-executor marginalia markdown-xwidget
+                math-delimiters mcp-server md-babel meow-tree-sitter
+                meow-vterm mermaid-mode minimap mistty
+                mu4e-column-faces mu4e-overview nerd-icons-corfu
+                nerd-icons-dired netease-cloud-music no-littering nov
+                ns-auto-titlebar ob-aider ob-applescript ob-async
+                ob-go ob-kotlin ob-powershell ob-rust ob-swiftui
+                ob-typescript org-agenda-count org-anki org-appear
+                org-cliplink org-cv org-dial org-drill org-edna
+                org-imgtog org-link-beautify org-media-note org-mode
+                org-modern org-modern-indent org-pdftools
+                org-remoteimg org-roam-bibtex org-roam-ui org-yt osm
+                osx-dictionary osx-location osx-plist ox-hugo ox-jira3
                 pangu-spacing pass password-store-menu pet
                 pinyin-isearch pinyin-search poly-ansible powershell
-                protobuf-mode pulsar quick-sdcv rainbow-csv
-                rainbow-mode rg rime rust-mode sideline-blame
-                sideline-eglot sideline-flycheck sideline-flymake
-                smartparens solaire-mode swift-ts-mode system-packages
-                telega templ-ts-mode templatel toc-mode
-                transpose-frame tree-sitter treemacs-magit
+                protobuf-mode pulsar rainbow-csv rainbow-mode rg rime
+                rust-mode sideline-eglot sideline-flycheck
+                sideline-flymake smartparens solaire-mode
+                swift-ts-mode system-packages telega templ-ts-mode
+                templatel toc-mode transpose-frame treemacs-magit
                 treemacs-nerd-icons treemacs-tab-bar treesit-fold
                 treesit-langs tributary ultra-scroll undo-fu-session
                 uv-mode valign vbs-repl vbscript-mode
