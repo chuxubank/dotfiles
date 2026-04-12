7bb8579fe88447c27d214234b4a11a753744473d
Misaka <chuxubank@qq.com>
2026-04-12 13:50:05 +0800

    refactor(config): add more custom variable settings
    Adds necessary settings for various packages to the emacs-custom.el file, ensuring proper initialization.

--- home/dot_config/emacs-custom.el
+++ home/dot_config/emacs-custom.el
@@ -4,7 +4,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(mouse-wheel-progressive-speed nil)
+ '(org-roam-mode-sections
+   (list #'org-roam-backlinks-section #'org-roam-reflinks-section
+         #'org-roam-unlinked-references-section) nil nil "Customized with use-package org-roam")
  '(package-selected-packages
    '(ace-pinyin activities agent-shell aider aidermacs anki-vocabulary
                 apheleia applescript-mode auctex avy-embark-collect
