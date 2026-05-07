a4eb6164f42ac3d2c41ec4364c81f97278fa519d
Misaka <chuxubank@qq.com>
2026-05-07 14:19:50 +0800

    chore(emacs): update package list

--- home/dot_config/emacs-custom.el
+++ home/dot_config/emacs-custom.el
@@ -17,7 +17,7 @@
                 chatgpt-shell chezmoi citar-embark citar-org-roam
                 cmake-font-lock code-review codeium consult-gh-embark
                 consult-gh-forge copilot corfu-prescient dart-mode
-                deft delight devdocs diagram-preview diff-hl
+                dashboard deft delight devdocs diagram-preview diff-hl
                 dired-rsync-transient dirvish docker
                 docker-compose-mode dockerfile-mode doom-modeline
                 doom-themes dumb-jump easy-hugo edit-indirect
@@ -25,34 +25,35 @@
                 elisp-demos elogcat emacs-everywhere ement emms
                 eshell-vterm exec-path-from-shell flycheck-gradle
                 flycheck-kotlin flycheck-plantuml flymake-ansible-lint
-                flymake-gradle git-link git-timemachine glab go-mode
-                go-template-helper-mode go-template-mode goggles gogs
-                gptel-magit gptel-prompts gradle-el graphql-mode
-                groovy-mode gt gtea helpful hugoista igist indent-bars
-                kaolin-themes kdl-mode kotlin-ts-mode leetcode
-                ligature logview lsp-proxy lua-mode magit-todos
-                major-mode-hydra makefile-executor marginalia
-                markdown-xwidget math-delimiters mcp-server md-babel
-                meow-tree-sitter meow-vterm mermaid-mode minimap
-                mistty mu4e-column-faces mu4e-overview
-                nerd-icons-corfu nerd-icons-dired netease-cloud-music
-                no-littering nov ns-auto-titlebar ob-aider
-                ob-applescript ob-async ob-go ob-kotlin ob-powershell
-                ob-rust ob-swiftui ob-typescript org-agenda-count
-                org-anki org-appear org-cliplink org-cv org-dial
-                org-drill org-edna org-imgtog org-link-beautify
-                org-media-note org-mode org-modern org-modern-indent
-                org-pdftools org-remoteimg org-roam-bibtex org-roam-ui
-                org-yt osm osx-dictionary osx-location osx-plist
-                ox-hugo pangu-spacing pass password-store-menu pet
+                flymake-bridge flymake-gradle git-link git-timemachine
+                glab go-mode go-template-helper-mode go-template-mode
+                goggles gogs gptel-magit gptel-prompts gradle-el
+                graphql-mode grip-mode groovy-mode gt gtea helpful
+                hugoista igist indent-bars kaolin-themes kdl-mode
+                kotlin-ts-mode leetcode ligature logview lsp-proxy
+                lua-mode magit-todos major-mode-hydra
+                makefile-executor marginalia markdown-xwidget
+                math-delimiters mcp-server md-babel meow-tree-sitter
+                meow-vterm mermaid-mode minimap mistty
+                mu4e-column-faces mu4e-overview nerd-icons-corfu
+                nerd-icons-dired netease-cloud-music no-littering nov
+                ns-auto-titlebar ob-aider ob-applescript ob-async
+                ob-go ob-kotlin ob-powershell ob-rust ob-swiftui
+                ob-typescript org-agenda-count org-anki org-appear
+                org-cliplink org-cv org-dial org-drill org-edna
+                org-imgtog org-link-beautify org-media-note org-mode
+                org-modern org-modern-indent org-pdftools
+                org-remoteimg org-roam-bibtex org-roam-ui org-yt osm
+                osx-dictionary osx-location osx-plist ox-hugo
+                pangu-spacing pass password-store-menu pet
                 pinyin-isearch pinyin-search poly-ansible powershell
                 protobuf-mode pulsar rainbow-csv rainbow-mode rg rime
                 rust-mode sideline-eglot sideline-flycheck
                 sideline-flymake smartparens solaire-mode
                 swift-ts-mode system-packages telega templ-ts-mode
                 templatel toc-mode transpose-frame treemacs-magit
-                treemacs-nerd-icons treemacs-tab-bar treesit-auto
-                treesit-fold tributary ultra-scroll undo-fu-session
+                treemacs-nerd-icons treemacs-tab-bar treesit-fold
+                treesit-langs tributary ultra-scroll undo-fu-session
                 uv-mode valign vbs-repl vbscript-mode
                 vertico-prescient video-trimmer visual-basic-mode
                 vterm-toggle vundo winum yaml-pro))
