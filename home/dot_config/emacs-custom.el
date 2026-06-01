caa0f22f4ecb71aedc926bc57ab5e44417879234
Misaka <chuxubank@qq.com>
2026-06-01 21:49:19 +0800

    chore(emacs): update package list, add gptel-model-updater

--- home/dot_config/emacs-custom.el
+++ home/dot_config/emacs-custom.el
@@ -29,25 +29,25 @@
                 flycheck-kotlin flycheck-plantuml flymake-ansible-lint
                 flymake-bridge flymake-gradle git-link git-timemachine
                 glab go-mode go-template-helper-mode go-template-mode
-                goggles gogs gptel-magit gptel-prompts gradle-el
-                graphql-mode grip-mode groovy-mode gt gtea helpful
-                hugoista igist indent-bars kaolin-themes kdl-mode
-                kotlin-mode kotlin-ts-mode leetcode ligature logview
-                lsp-proxy lua-mode magit-todos major-mode-hydra
-                makefile-executor marginalia markdown-xwidget
-                math-delimiters mcp-server md-babel meow-tree-sitter
-                meow-vterm mermaid-mode minimap mistty
-                mu4e-column-faces mu4e-overview nerd-icons-corfu
-                nerd-icons-dired netease-cloud-music no-littering nov
-                ns-auto-titlebar ob-aider ob-applescript ob-async
-                ob-go ob-kotlin ob-powershell ob-rust ob-swiftui
-                ob-typescript org-agenda-count org-anki org-appear
-                org-cliplink org-cv org-dial org-drill org-edna
-                org-imgtog org-link-beautify org-media-note org-mode
-                org-modern org-modern-indent org-pdftools
-                org-remoteimg org-roam-bibtex org-roam-ui org-yt osm
-                osx-dictionary osx-location osx-plist ox-hugo
-                pangu-spacing pass password-store-menu pet
+                goggles gogs gptel-magit gptel-model-updater
+                gptel-prompts gradle-el graphql-mode grip-mode
+                groovy-mode gt gtea helpful hugoista igist indent-bars
+                kaolin-themes kdl-mode kotlin-mode kotlin-ts-mode
+                leetcode ligature logview lsp-proxy lua-mode
+                magit-todos major-mode-hydra makefile-executor
+                marginalia markdown-xwidget math-delimiters mcp-server
+                md-babel meow-tree-sitter meow-vterm mermaid-mode
+                minimap mistty mu4e-column-faces mu4e-overview
+                nerd-icons-corfu nerd-icons-dired netease-cloud-music
+                no-littering nov ns-auto-titlebar ob-aider
+                ob-applescript ob-async ob-go ob-kotlin ob-powershell
+                ob-rust ob-swiftui ob-typescript org-agenda-count
+                org-anki org-appear org-cliplink org-cv org-dial
+                org-drill org-edna org-imgtog org-link-beautify
+                org-media-note org-mode org-modern org-modern-indent
+                org-pdftools org-remoteimg org-roam-bibtex org-roam-ui
+                org-yt osm osx-dictionary osx-location osx-plist
+                ox-hugo pangu-spacing pass password-store-menu pet
                 pinyin-isearch pinyin-search poly-ansible powershell
                 protobuf-mode pulsar rainbow-csv rainbow-mode rg rime
                 rust-mode sideline-eglot sideline-flycheck
