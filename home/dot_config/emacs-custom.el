5904fb983b64a91c3d59a08293ffe49fef568bcf
Misaka <chuxubank@qq.com>
2026-06-24 17:07:53 +0800

    chore(emacs): add missing packages and reorder custom.el list

--- home/dot_config/emacs-custom.el
+++ home/dot_config/emacs-custom.el
@@ -30,37 +30,38 @@
                 flycheck-kotlin flycheck-plantuml flymake-ansible-lint
                 flymake-gradle ghostel git-link git-timemachine glab
                 go-mode go-template-helper-mode go-template-mode
-                goggles gogs gptel-magit gptel-model-updater
-                gptel-prompts gradle-el graphql-mode grip-mode
-                groovy-mode gt gtea helpful hugoista igist indent-bars
-                kaolin-themes kdl-mode keycast kitty-graphics
-                kotlin-mode kotlin-ts-mode leetcode ligature logview
-                lsp-proxy lua-mode magit-todos major-mode-hydra
-                makefile-executor marginalia markdown-xwidget
-                math-delimiters mcp-server md-babel meow-tree-sitter
-                meow-vterm mermaid-mode minimap mistty
-                mu4e-column-faces mu4e-overview nerd-icons-corfu
-                nerd-icons-dired netease-cloud-music no-littering nov
-                ns-auto-titlebar ob-aider ob-applescript ob-async
-                ob-go ob-kotlin ob-powershell ob-rust ob-swiftui
-                ob-typescript org-agenda-count org-anki org-appear
-                org-cliplink org-cv org-dial org-drill org-edna
-                org-imgtog org-link-beautify org-media-note org-mode
-                org-modern org-modern-indent org-pdftools
-                org-remoteimg org-roam-bibtex org-roam-ui org-yt osm
-                osx-dictionary osx-location osx-plist ox-hugo
-                pangu-spacing pass password-store-menu pet
-                pinyin-isearch pinyin-search poly-ansible powershell
-                protobuf-mode pulsar rainbow-csv rainbow-mode rg rime
-                rust-mode sideline-eglot sideline-flycheck
-                sideline-flymake smartparens solaire-mode
-                swift-ts-mode system-packages telega templ-ts-mode
-                templatel toc-mode transpose-frame treemacs-magit
-                treemacs-nerd-icons treemacs-tab-bar treesit-fold
-                treesit-langs tributary ultra-scroll undo-fu-session
-                uv-mode valign vbs-repl vbscript-mode
-                vertico-prescient video-trimmer visual-basic-mode vlf
-                vterm-toggle vundo winum yaml-pro))
+                goggles gogs gptel-forge-prs gptel-magit
+                gptel-model-updater gptel-prompts gradle-el
+                graphql-mode grip-mode groovy-mode gt gtea helpful
+                hugoista igist indent-bars kaolin-themes kdl-mode
+                keycast kitty-graphics kotlin-mode kotlin-ts-mode
+                leetcode ligature llm logview lsp-proxy lua-mode
+                magit-todos major-mode-hydra makefile-executor
+                marginalia markdown-xwidget math-delimiters mcp-server
+                md-babel meow-tree-sitter meow-vterm mermaid-mode
+                minimap mistty mu4e-column-faces mu4e-overview
+                nerd-icons-corfu nerd-icons-dired netease-cloud-music
+                no-littering nov ns-auto-titlebar ob-aider
+                ob-applescript ob-async ob-go ob-kotlin ob-powershell
+                ob-rust ob-swiftui ob-typescript org-agenda-count
+                org-anki org-appear org-cliplink org-cv org-dial
+                org-drill org-edna org-imgtog org-link-beautify
+                org-media-note org-mode org-modern org-modern-indent
+                org-pdftools org-remoteimg org-roam-bibtex org-roam-ui
+                org-yt osm osx-dictionary osx-location osx-plist
+                ox-hugo package-lint pangu-spacing pass
+                password-store-menu pet pinyin-isearch pinyin-search
+                poly-ansible powershell protobuf-mode pulsar
+                rainbow-csv rainbow-mode rg rime rust-mode
+                sideline-eglot sideline-flycheck sideline-flymake
+                smartparens solaire-mode swift-ts-mode system-packages
+                telega templ-ts-mode templatel toc-mode
+                transpose-frame treemacs-magit treemacs-nerd-icons
+                treemacs-tab-bar treesit-fold treesit-langs tributary
+                ultra-scroll undo-fu-session uv-mode valign vbs-repl
+                vbscript-mode vertico-prescient video-trimmer
+                visual-basic-mode vlf vterm-toggle vundo winum
+                yaml-pro yasnippet))
  '(safe-local-variable-values
    '((org-highlight-latex-and-related) (org-blank-before-new-entry))))
 (custom-set-faces
