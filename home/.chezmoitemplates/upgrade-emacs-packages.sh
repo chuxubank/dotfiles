b5cc157bc17db3477e938eb7c67af23ec5db18ac
Misaka <chuxubank@qq.com>
2026-07-06 12:02:55 +0800

    fix(emacs): set CI env var in same command line as emacs daemon invocation

--- home/.chezmoitemplates/upgrade-emacs-packages.sh
+++ home/.chezmoitemplates/upgrade-emacs-packages.sh
@@ -1,7 +1,5 @@
-CI=true
-
-yes | \emacs --fg-daemon \
-    --eval '(package-install-selected-packages t)' \
-    --eval '(when package-selected-packages (package-autoremove))' \
-    --eval '(package-upgrade-all)' \
-    --eval '(kill-emacs)'
+CI=true yes | \emacs --fg-daemon \
+        --eval '(package-install-selected-packages t)' \
+        --eval '(when package-selected-packages (package-autoremove))' \
+        --eval '(package-upgrade-all)' \
+        --eval '(kill-emacs)'
