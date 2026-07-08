5d61f1357731841f6ed28ab61e35542f871ebb77
Misaka <chuxubank@qq.com>
2026-07-08 19:16:39 +0800

    feat(emacs): refactor fill-list to accept indent as number and add package-vc-selected-packages formatting
    - Change `fill-list` template to accept `indent` as a number of spaces instead of a literal string
    - Add proper formatting for `package-vc-selected-packages` using the same fill-list template
    - Fix continuation line indentation to be calculated dynamically based on package name length

--- home/dot_config/cat-emacs/modify_custom.el
+++ home/dot_config/cat-emacs/modify_custom.el
@@ -6,7 +6,23 @@
 {{- /* Fill package-selected-packages to fill-column like Emacs does:
        first token after "   '(", continuation lines indented 16 spaces,
        wrapping at 70 columns. */ -}}
-{{- $selBody := includeTemplate "emacs/fill-list" (dict "items" $selected "width" 70 "first" "   '(" "indent" "                " "suffix" "))") -}}
+{{- $selBody := includeTemplate "emacs/fill-list" (dict "items" $selected "width" 70 "first" "   '(" "indent" 16 "suffix" "))") -}}
+{{- /* Fill package-vc-selected-packages the same way: each element is a
+       plist (name :url URL [:lisp-dir DIR] [:branch B]) wrapped at 70
+       columns, continuation lines aligned under the first token. */ -}}
+{{- $vcElems := list -}}
+{{- range $p := $vc -}}
+{{-   $toks := list ":url" (printf "%q" $p.url) -}}
+{{-   if hasKey $p "lisp_dir" }}{{ $toks = concat $toks (list ":lisp-dir" (printf "%q" $p.lisp_dir)) }}{{ end -}}
+{{-   if hasKey $p "branch" }}{{ $toks = concat $toks (list ":branch" (printf "%q" $p.branch)) }}{{ end -}}
+{{-   $elem := includeTemplate "emacs/fill-list" (dict "items" $toks "width" 65 "first" (printf "(%s " $p.name) "indent" (add (len $p.name) 7) "suffix" ")") -}}
+{{-   $vcElems = append $vcElems $elem -}}
+{{- end -}}
+{{- $vcBody := "" -}}
+{{- range $i, $e := $vcElems -}}
+{{-   if eq $i 0 }}{{ $vcBody = printf "   '(%s" $e }}{{ else }}{{ $vcBody = printf "%s\n     %s" $vcBody $e }}{{ end -}}
+{{- end -}}
+{{- $vcBody = printf " '(package-vc-selected-packages\n%s))" $vcBody -}}
 ;;; -*- lexical-binding: t -*-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -25,11 +41,7 @@
      {{- end }}))
  '(package-selected-packages
 {{ $selBody }}
- '(package-vc-selected-packages
-   '(
-     {{- range $p := $vc }}
-     ({{ $p.name }} . (:url "{{ $p.url }}"{{ if hasKey $p "lisp_dir" }} :lisp-dir "{{ $p.lisp_dir }}"{{ end }}{{ if hasKey $p "branch" }} :branch "{{ $p.branch }}"{{ end }}))
-     {{- end }}))
+{{ $vcBody }}
  '(safe-local-variable-values
    '((org-highlight-latex-and-related) (org-blank-before-new-entry))))
 (custom-set-faces
