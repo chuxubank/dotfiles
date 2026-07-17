2e4d72a79d55c55244b1063d026d0deb0c62c241
Misaka <chuxubank@qq.com>
2026-07-17 23:34:33 +0800

    feat(emacs): use forked chezmoi template packages

--- home/.chezmoitemplates/emacs/custom-packages.el
+++ home/.chezmoitemplates/emacs/custom-packages.el
@@ -6,12 +6,13 @@ first token after "   '(", continuation lines indented 16 spaces,
 wrapping at 70 columns. */ -}}
 {{- $selBody := includeTemplate "emacs/fill-list" (dict "items" $selected "width" 70 "first" "   '(" "indent" 16 "suffix" ")") -}}
 {{- /* Fill package-vc-selected-packages the same way: each element is a
-plist (name :url URL [:lisp-dir DIR] [:branch B]) wrapped at 70
+plist (name :url URL [:lisp-dir DIR] [:main-file FILE] [:branch B]) wrapped at 70
 columns, continuation lines aligned under the first token. */ -}}
 {{- $vcElems := list -}}
 {{- range $p := $vc -}}
 {{-   $toks := list ":url" (printf "%q" $p.url) -}}
 {{-   if hasKey $p "lisp_dir" }}{{ $toks = concat $toks (list ":lisp-dir" (printf "%q" $p.lisp_dir)) }}{{ end -}}
+{{-   if hasKey $p "main_file" }}{{ $toks = concat $toks (list ":main-file" (printf "%q" $p.main_file)) }}{{ end -}}
 {{-   if hasKey $p "branch" }}{{ $toks = concat $toks (list ":branch" (printf "%q" $p.branch)) }}{{ end -}}
 {{-   $elem := includeTemplate "emacs/fill-list" (dict "items" $toks "width" 65 "first" (printf "(%s " $p.name) "indent" (add (len $p.name) 7) "suffix" ")") -}}
 {{-   $vcElems = append $vcElems $elem -}}
