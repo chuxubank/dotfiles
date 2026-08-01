7eb422e6ce5168da6a7f97a479e4a561dc187d2f
Misaka <chuxubank@qq.com>
2026-08-01 17:12:18 +0800

    chore: simplify upgrade-packages script by removing unnecessary env wrapper

--- home/.chezmoitemplates/emacs/upgrade-packages.sh
+++ home/.chezmoitemplates/emacs/upgrade-packages.sh
@@ -1,1 +1,1 @@
-env -u CI TERM_PROGRAM=chezmoi make -C {{ joinPath .chezmoi.homeDir .path.personal.emacs }} sync-upgrade-packages
+make -C {{ joinPath .chezmoi.homeDir .path.personal.emacs }} sync-upgrade-packages
